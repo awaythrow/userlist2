1032ec2cac9833b0f2ab5cadf5b53fab636f90c1
TJ Miller <millert@us.ibm.com>
2020-11-06 15:00:31 -0800

    Add users model and controller methods

--- app.rb
+++ app.rb
@@ -6,4 +6,33 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/users' do
+    users = Users.all
+    json users
+  end
+
+  get '/users/:id' do
+    user = Users.find(params[:id])
+    json user
+  end
+
+  post '/users' do
+    user = Users.create(params)
+    status 201
+    json user
+  end
+
+  put '/users/:id' do
+    user = Users.find(params[:id])
+    user.update(params)
+    status 204
+    json user
+  end
+
+  delete '/users/:id' do
+    user = Users.destroy(params[:id])
+    status 202
+    json user
+  end
+
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,4 +8,59 @@ describe App do
     expect(last_response.status).to eq(200)
     expect(last_response.body).to include("Welcome to the Sinatra Template!")
   end
+
+  describe "CRUD users" do
+    it "returns valid json" do
+      get "/users"
+      expect(last_response).to be_ok
+      expect(last_response.header['Content-Type']).to include 'application/json'
+      expect(JSON.parse last_response.body).to_not be_nil
+    end
+
+    context "create several users" do
+      before do
+        @users = [
+          { firstname: 'user', lastname: 'one', email: 'userone@example.com' },
+          { firstname: 'user', lastname: 'two', email: 'usertwo@example.com' },
+          { firstname: 'user', lastname: 'three', email: 'userthree@example.com' },
+        ]
+        @users.map! { |user| user.transform_keys(&:to_s) }
+        @users.each do |user|
+          post "/users", user
+        end
+      end
+      it "returns all the users" do
+        get "/users"
+        expect(last_response).to be_ok
+
+        response_data = JSON.parse(last_response.body)
+        response_data.each { |h| h.delete("id") }
+        expect(response_data).to include(*@users)
+      end
+    end
+
+    context "delete users" do
+      before do
+        @users = [
+          { firstname: 'user', lastname: 'one', email: 'userone@example.com' },
+          { firstname: 'user', lastname: 'two', email: 'usertwo@example.com' },
+          { firstname: 'user', lastname: 'three', email: 'userthree@example.com' },
+        ]
+        @users.map! { |user| user.transform_keys(&:to_s) }
+        @users.each do |user|
+          post "/users", user
+        end
+      end
+      it "deletes users" do
+        get "/users"
+        expect(last_response).to be_ok
+
+        response_data = JSON.parse(last_response.body)
+        response_data.each do |user|
+          delete "users/#{user['id']}"
+          expect(last_response.status).to eq 202
+        end
+      end
+    end
+  end
 end
